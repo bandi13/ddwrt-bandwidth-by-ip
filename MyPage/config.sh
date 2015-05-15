f0892309f6c625792c63b958f6a40eca7bb83491
Andras Fekete <bandi13@users.noreply.github.com>
2015-05-15 18:35:13 -0400

    Adding in some comments to each variable.

--- MyPage/config.sh
+++ MyPage/config.sh
@@ -1,3 +1,6 @@
+# List all the clients on the network that are utilizing bandwidth.
 DO_CLIENTS=1                                                                                                                        
+# Show a total bandwidth utilization by client.
 DO_BANDWSUM=1                                                                                                                       
+# This is a very CPU intensive procedure. Enable it if your router is beefy enough or for high number of connections, you'll have problems.
 DO_ACTIVE=0
